0eca6d1989c0109df40e1d956f9f020bcd4d331d
nix202 <earcanix@gmail.cm>
2023-11-17 15:16:36 -0500

    Verification

--- Verification.php
+++ Verification.php
@@ -1,6 +1,25 @@
 <?php
 include_once ("functions.php");
-var_dump($_POST);
-$asada = buildPostAddressList();
-var_dump($asada);
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <title>Verification</title>
+</head>
+<body>
+<form action="Sauvegarde.php" method="POST">
+<?php
+foreach($_POST as $id => $address) {
+    echo addressFields($id, $address, true);
+    echo "<br>";
+}
+?>
+    <a href="#" class="btn btn-primary" onclick="history.back()"> Precedent </a>
+    <button class="btn btn-primary" type="submit" > Confirmer  </button>
+</form>
+</body>
+</html>
